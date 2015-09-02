73be0d04c1f5c7b7003f4da295bfc871f49a8fda
Jay <jayneedle32@yahoo.com>
2015-09-01 17:31:47 -0700

    Create calc.rb

---
+++ calc.rb
@@ -0,0 +1,15 @@
+hours_in_year = 365 * 24
+
+puts hours_in_year
+
+minutes_in_decade = hours_in_year * 10 * 60
+
+puts minutes_in_decade
+
+age_in_seconds = hours_in_year * 26 * 60 * 60
+
+puts age_in_seconds
+
+how_old = 1233000000000 / 60 / 60 / 24 / 365
+
+puts how_old
